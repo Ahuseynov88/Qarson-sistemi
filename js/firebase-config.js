3bc76ccd8508ba7e4d72e54ed19105f85eaeb3c1
Ahuseynov88 <ahuseynov1988@gmail.com>
2026-06-21 12:57:57 +0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -10,9 +10,10 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
 const R = {
-  waiters:    db.ref('waiters'),
-  tables:     db.ref('tables'),
-  orders:     db.ref('orders'),
-  logs:       db.ref('logs'),
-  menuItems:  db.ref('menuItems')
+  waiters:     db.ref('waiters'),
+  tables:      db.ref('tables'),
+  orders:      db.ref('orders'),
+  logs:        db.ref('logs'),
+  menuItems:   db.ref('menuItems'),
+  tableOrders: db.ref('tableOrders')
 };
