6ca2eb61a053982627d7c1ac68b753b0fbfe6a42
Ahuseynov88 <ahuseynov1988@gmail.com>
2026-08-26 14:16:28 +0400

    Update kitchen.js

--- js/kitchen.js
+++ js/kitchen.js
@@ -103,7 +103,7 @@ function renderKitchenCard(ko) {
     </div>
     <div class="ko-waiter-name"><svg class="icon" style="width:.85em;height:.85em;"><use href="#i-user"></use></svg> ${esc(ko.waiterName || '')}</div>
       const acceptBtn = !kitchenAccepted
-    ? `<button class="ko-accept-btn" onclick="kitchenAcceptOrder('${esc(ko.id)}')">✓ Qəbul et</button>` : '';
+    <button class="ko-accept-btn" onclick="kitchenAcceptOrder('${esc(ko.id)}')">✓ Qəbul et</button>` : '';
         ${acceptBtn}${headerStatus}${changeNotice}${orderNote}
     <div class="ko-items">${itemsHtml}</div>
   </div>`;
